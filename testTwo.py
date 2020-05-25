538996287fe0884529527fc68e200138c9e9eab0
Danzil1 <danzil.fernandes@sjinnovation.com>
2020-05-25 11:24:08 +0530

    Added new file to fail

---
+++ testTwo.py
@@ -0,0 +1,6 @@
+for x in range(6):
+    printr("Welcome to Jenkins github Integration " + str(x))
+else:
+    printr("Yes. You successfully completed the first build.")
+    printr("You completed more than 50%. Great job")
+    printr("Testing for crons on deployment")
